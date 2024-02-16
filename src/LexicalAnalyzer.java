1f6c6d4aa1356bf9c32b1109bec67874b9dd4092
ThanhDat22 <dat.nguyen2284@gmail.com>
2024-02-16 03:31:24 -0600

    Add front.in and main program

--- src/LexicalAnalyzer.java
+++ src/LexicalAnalyzer.java
@@ -1,6 +1,24 @@
-public class LexicalAnalyzer {
-    public static void main(String[] args) {
-        // Open the input data file and process its contents
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class LexicalAnalyzer {
+
+    // Class Methods
+
+    /** Gets the next charater of input and determine its character class */
+    private static void getChar() {
+        int nextCharInt = 
+        if(scanner.)
+
+    }
+    public static void main(String[] args) throws IOException {
+        // Open the input data file and process its contents
+        FileInputStream inputFile = new FileInputStream("front.in");
+        Scanner scanner = new Scanner(inputFile);
+        getChar();
+        do {
+            lex();
+        } while (nextToken != -1);
     }
 }
